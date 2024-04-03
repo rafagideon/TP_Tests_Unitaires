1072ae75ced30d33da8c2b29eda3965d2402cb09
RFG <RFG@RFG-PC>
2024-04-03 21:00:54 +0200

    02)

--- src/fr/emse/test/MoneyTest.java
+++ src/fr/emse/test/MoneyTest.java
@@ -7,8 +7,12 @@ import org.junit.Test;
 public class MoneyTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testSimpleAdd() {
+		Money m12CHF = new Money(12, "CHF"); // création de données
+		Money m14CHF = new Money(14, "CHF");
+		Money expected = new Money(26, "CHF");
+		Money result = m12CHF.add(m14CHF); // exécution de la méthode testée
+		assertTrue(expected.equals(result)); // comparaison
 	}
 
 }
