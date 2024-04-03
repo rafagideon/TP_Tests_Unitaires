56da6e1edb82e9590634443757c6c2f5f0593231
RFG <RFG@RFG-PC>
2024-04-03 20:53:53 +0200

    01) Création du projet et des classes Money et MoneyTest

---
+++ src/fr/emse/test/MoneyTest.java
@@ -0,0 +1,14 @@
+package fr.emse.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MoneyTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
